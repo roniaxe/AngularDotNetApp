e4ab8354b7864112c6eb64061db3da616b32cf04
roniaxe <roniaxe@gmail.com>
2018-10-20 17:18:04 -0400

    Added JWT Authorization

--- TestWebApi/Controllers/AuthController.cs
+++ TestWebApi/Controllers/AuthController.cs
@@ -1,8 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using TestWebApi.Data;
 using TestWebApi.DTOs;
 using TestWebApi.Models;
@@ -14,13 +17,15 @@ namespace TestWebApi.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _authRepository;
+        private readonly IConfiguration _config;
 
-        public AuthController(IAuthRepository authRepository)
+        public AuthController(IAuthRepository authRepository, IConfiguration config)
         {
             _authRepository = authRepository;
+            _config = config;
         }
 
-        [HttpPost]
+        [HttpPost("register")]
         public async Task<IActionResult> Register(UserForRegisterDto userDto)
         {
             // validate request
@@ -37,13 +42,41 @@ namespace TestWebApi.Controllers
 
             return StatusCode(201);
         }
-        
+
         [HttpPost("login")]
-        public async Task<IActionResult> Login(UserForRegisterDto userDto)
+        public async Task<IActionResult> Login(UserForLoginDto userDto)
         {
-            var loginResult = await _authRepository.Login(userDto.Username, userDto.Password);
-            if (loginResult == null) return BadRequest("No username exists");
-            return Ok(loginResult);
+            var u = User.Identity;
+
+            var user = await _authRepository.Login(userDto.Username.ToLower(), userDto.Password);
+            if (user == null) return Unauthorized();
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                SigningCredentials = cred,
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(1)
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new
+            {
+                token = tokenHandler.WriteToken(token),
+                exp = token.ValidTo.ToLocalTime()
+            });
         }
     }
 }
